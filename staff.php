a9a735b027d180a056c6a2477bbb63585c06c25c
CQQN_Spark <77710996+CQQN-Spark@users.noreply.github.com>
2022-04-20 19:58:50 -0500

    Add files via upload

---
+++ staff.php
@@ -0,0 +1,81 @@
+<!DOCTYPE php>
+<php lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <title>TWS</title>
+        <!-- Bootstrap core CSS -->
+        <link href="assets/css/bootstrap.css" rel="stylesheet">
+		<!-- MyMC CSS -->
+        <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet">
+		<!-- FontAwesome CSS -->
+        <link href="assets/css/mymc.css" rel="stylesheet">
+    </head>
+    <body>
+        <nav class="navbar navbar-default navbar-static-top" role="navigation">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    </button>
+                    <!-- You'll want to use a responsive image option so this logo looks good on devices - I recommend using something like retina.js (do a quick Google search for it and you'll find it) -->
+                    <a class="navbar-brand" href="index.php">TWS</a>
+                </div>
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse navbar-ex1-collapse">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="index.php"><i class="fa fa-home"></i> Home</a></li>
+                        <li><a href="rules.php"><i class="fa fa-info-circle"></i> Rules</a></li>
+                        <li class="active"><a href="staff.php"><i class="fa fa-star"></i> Staff</a></li>
+                        <li><a href="nations.php"><i class="fa fa-check"></i> Nations</a></li>
+                    </ul>
+                </div>
+                <!-- /.navbar-collapse -->
+            </div>
+            <!-- /.owner -->
+        </nav>
+            <div class="container">
+			<h1 class="page-header">Owner</h1>
+					<div style="background: url(https://raw.githubusercontent.com/CQQN-Spark/TWS-STAFF/main/CQQN_Spark.png);" class="staff-member">
+						<div class="staff-name">
+							<span>CQQN_Spark</span>
+						</div>
+					</div>
+		    
+	    </div>
+	    <!-- /.co owner -->
+	</nav>
+            <div class="container">
+			<h1 class="page-header">Co Owner</h1>
+					<div style="background: url(https://raw.githubusercontent.com/CQQN-Spark/TWS-STAFF/main/Demigod4591.png);" class="staff-member">
+						<div class="staff-name">
+							<span>Demigod4591</span>
+						</div>
+					</div>
+	    </div>
+			<!-- /.container -->
+        <div class="container">
+            <hr>
+            <footer>
+                <div class="row">
+                    <div class="col-lg-12">
+                        <ul class="nav nav-pills bottom">
+						  <li role="presentation"><a href="staff.php">Staff</a></li>
+						  <li role="presentation"><a href="rules.php">Rules</a></li>
+						  <li class="pull-right">&copy;2022 TWS</li>
+						</ul>
+					</div>
+                </div>
+            </footer>
+        </div>
+        <!-- /.container -->
+        <!-- JavaScript -->
+        <script src="assets/js/jquery-1.10.2.js"></script>
+        <script src="assets/js/bootstrap.js"></script>
+    </body>
+</php>
